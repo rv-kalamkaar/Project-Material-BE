bed13562e4445443484226acff319d119d0c0368
rv-kalamkaar <rohankumarkhare@gmail.com>
2023-09-11 14:19:31 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -4,8 +4,37 @@ import pickle as pkl
 
 # importing pickle files
 
-diabetes_model = pkl.load(open("D:/Rohan Vikramaditya/Project Material BE/diabetes_model.sav"))
+diabetes_model = pkl.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pkl.load(open("D:/Rohan Vikramaditya/Project Material BE/heart_disease_model.sav"))
+heart_disease_model = pkl.load(open('heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pkl.load(open("D:/Rohan Vikramaditya/Project Material BE\parkinsons_model.sav"))
+parkinsons_model = pkl.load(open('parkinsons_model.sav', 'rb'))
+
+
+# sidebar for nav
+
+with st.sidebar:
+
+    selected = option_menu("Health-Guard: Machine learning driven – disease prediction and personalized recommendation system",
+                           ['Diabetes Prediction System',
+                            'Heart Disease Prediction System',
+                            'Parkinsons Disease Prediction System'],
+                            default_index=0)
+    
+
+# diabetes prediction rate
+
+if (selected=="Diabetes Prediction System"):
+
+    #page title
+    st.title('Diabetes Prediction System')
+
+if (selected=="Heart Disease Prediction System"):
+
+    #page title
+    st.title('Heart Disease Prediction System')
+
+if (selected=="Parkinsons Disease Prediction System"):
+
+    #page title
+    st.title('Parkinsons Disease Prediction System')

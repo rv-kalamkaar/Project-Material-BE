d3eb7672d1a18e4cd63b07e41e8e3f14ad876e17
Kunalsrp <pattanshettikunal0201@gmail.com>
2023-09-13 20:45:00 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -19,70 +19,188 @@ with st.sidebar:
                            ['Diabetes Prediction System',
                             'Heart Disease Prediction System',
                             'Parkinsons Disease Prediction System'],
-                           icons=['activity', 'heart', 'person'],
-                           default_index=0)
+                            icons =['activity','heart','person'],
+                            default_index=0)
+    
 
+#*****************************************DIABETES SECTION********************************************
 
-# diabetes prediction rate
+if (selected=="Diabetes Prediction System"):
 
-if (selected == "Diabetes Prediction System"):
-
-    # page title
+    #page title
     st.title('Diabetes Prediction System')
-    # getting input data from the user
-    # Columns for input fields
+
+    #getting input data from the user
+    #Columns for input fields
 
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        Pregnancies = st.number_input('No of pregnancies', step=1)
+        Pregnancies = st.number_input('No of pregnancies')
 
     with col2:
-        Glucose = st.number_input('Glucose Level',step=5)
+        Glucose = st.number_input('Glucose Level')
 
     with col3:
-        BloodPressure = st.number_input('Blood Pressure value',step=5)
+        BloodPressure = st.number_input('Blood Pressure value')
 
     with col1:
-        Skinthickness = st.number_input('Skin Thickness value',step=5)
+        Skinthickness = st.text_input('Skin Thickness value')
 
     with col2:
-        Insulin = st.number_input('Insulin Level',step=1)
+        Insulin = st.text_input('Insulin Level')
 
     with col3:
-        bmi_index = st.number_input('BMI value',step=5)
+        bmi_index = st.text_input('BMI value')
 
     with col1:
-        DiabetesPedigreeFunction = st.number_input(
-            "Diabetes Pedigree Function value",step=0.1)
+        DiabetesPedigreeFunction = st.text_input("Diabetes Pedigree Function value")
 
     with col2:
-        Age = st.number_input("Age of a person",step=1)
+        Age = st.text_input("Age of a person")
 
-    # code for prediction
+
+
+
+    #code for prediction
     diab_diagnosis = ' '
 
-    # creating a button for prediction
+    #creating a button for prediction
 
     if st.button('Diabetes Test Result'):
-        inputFeatures = [Pregnancies,Glucose,BloodPressure,Skinthickness,Insulin,bmi_index, DiabetesPedigreeFunction, Age]
-        diab_prediction = diabetes_model.predict([inputFeatures])
+        diab_prediction = diabetes_model.predict([['Pregnancies','Glucose','BloodPressure','Skinthickness','Insulin','BMI','DiabetesPedigreeFunction','Age']])
 
-        if (diab_prediction[0] == 1):
+        if (diab_prediction[0]==1):
             diab_diagnosis = "The person is Diabetic"
         else:
             diab_diagnosis = "The person is not Diabetic"
 
     st.success(diab_diagnosis)
 
-# Heart Disease Prediction Rate
-if (selected == "Heart Disease Prediction System"):
 
-    # page title
+
+#*****************************************HEART SECTION********************************************
+
+
+# Heart Disease Prediction Rate
+if (selected=="Heart Disease Prediction System"):
+
+    #page title
     st.title('Heart Disease Prediction System')
 
-# Parkinsons Disease Prediction Rate
-if (selected == "Parkinsons Disease Prediction System"):
+    #getting input data from the user
+    #Columns for input fields
 
-    # page title
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        age = st.text_input('Age of person')
+
+    with col2:
+        sex = st.text_input('Sex of person [Male = 1 / Female = 0 ] ')
+
+    with col3:
+        cp = st.text_input('Chest Pain type')
+
+    with col1:
+        trestbps = st.text_input('Resting blood pressure')
+
+    with col2:
+        chol = st.text_input('Cholestrol in mg/dl')
+
+    with col3:
+        fbs = st.text_input('Fasting blood pressure')
+
+    with col1:
+         restecg = st.text_input("Resting electrographic result")
+
+    with col2:
+        talach = st.text_input("Maximum heart rate achieved")
+
+    with col3:
+        exang = st.text_input("Exercise induced angina")
+
+    with col1:
+        oldpeak = st.text_input("ST depression induced by exercise relative to rest")
+
+    with col2:
+        slope = st.text_input("Slope of the peak exercise ST segment")
+
+    with col3:
+        ca = st.text_input("Number of major vessels (0-3) colored by flourosopy")
+
+    with col1:
+        thal = st.text_input("Thal: 0 = normal; 1 = fixed defect; 2 = reversable defect")
+
+
+
+    #code for prediction
+    heart_diagnosis = ' '
+
+    #creating a button for prediction
+
+    if st.button('Diabetes Test Result'):
+        heart_prediction = heart_disease_model.predict([['Pregnancies','Glucose','BloodPressure','Skinthickness','Insulin','BMI','DiabetesPedigreeFunction','Age']])
+
+        if (diab_prediction[0]==1):
+            heart_diagnosis = "The person is Diabetic"
+        else:
+            heart_diagnosis = "The person is not Diabetic"
+
+    st.success(heart_diagnosis)
+
+
+    #*****************************************PARKISON SECTION********************************************
+
+# Parkinsons Disease Prediction Rate
+if (selected=="Parkinsons Disease Prediction System"):
+
+    #page title
     st.title('Parkinsons Disease Prediction System')
+
+    #getting input data from the user
+    #Columns for input fields
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        Pregnancies = st.number_input('No of pregnancies')
+
+    with col2:
+        Glucose = st.number_input('Glucose Level')
+
+    with col3:
+        BloodPressure = st.number_input('Blood Pressure value')
+
+    with col1:
+        Skinthickness = st.text_input('Skin Thickness value')
+
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+
+    with col3:
+        bmi_index = st.text_input('BMI value')
+
+    with col1:
+        DiabetesPedigreeFunction = st.text_input("Diabetes Pedigree Function value")
+
+    with col2:
+        Age = st.text_input("Age of a person")
+
+
+
+
+    #code for prediction
+    diab_diagnosis = ' '
+
+    #creating a button for prediction
+
+    if st.button('Diabetes Test Result'):
+        diab_prediction = diabetes_model.predict([['Pregnancies','Glucose','BloodPressure','Skinthickness','Insulin','BMI','DiabetesPedigreeFunction','Age']])
+
+        if (diab_prediction[0]==1):
+            diab_diagnosis = "The person is Diabetic"
+        else:
+            diab_diagnosis = "The person is not Diabetic"
+
+    st.success(diab_diagnosis)

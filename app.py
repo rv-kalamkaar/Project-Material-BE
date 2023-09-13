9b3051483857be2d74cf7375f8800fd6364b4dc5
rv-kalamkaar <rohankumarkhare@gmail.com>
2023-09-13 12:21:11 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -19,30 +19,70 @@ with st.sidebar:
                            ['Diabetes Prediction System',
                             'Heart Disease Prediction System',
                             'Parkinsons Disease Prediction System'],
-                            icons =['activity','heart','person'],
-                            default_index=0)
-    
+                           icons=['activity', 'heart', 'person'],
+                           default_index=0)
+
 
 # diabetes prediction rate
 
-if (selected=="Diabetes Prediction System"):
+if (selected == "Diabetes Prediction System"):
 
-    #page title
+    # page title
     st.title('Diabetes Prediction System')
+    # getting input data from the user
+    # Columns for input fields
 
-    Pregnancy = st.text_input('No of pregnancies')
-    Glucose = st.text_input('Glucose Level')
-    abc = st.file_uploader('upload data', accept_multiple_files=True, type=['png','jpeg'])
-    Temperature = st.text_input("Temperature")
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        Pregnancies = float(st.number_input('No of pregnancies'))
+
+    with col2:
+        Glucose = int(st.number_input('Glucose Level'))
+
+    with col3:
+        BloodPressure = int(st.number_input('Blood Pressure value'))
+
+    with col1:
+        Skinthickness = st.text_input('Skin Thickness value')
+
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+
+    with col3:
+        bmi_index = st.text_input('BMI value')
+
+    with col1:
+        DiabetesPedigreeFunction = st.text_input(
+            "Diabetes Pedigree Function value")
+
+    with col2:
+        Age = st.text_input("Age of a person")
+
+    # code for prediction
+    diab_diagnosis = ' '
+
+    # creating a button for prediction
+
+    if st.button('Diabetes Test Result'):
+        inputFeatures = [Pregnancies,Glucose,BloodPressure,Skinthickness,Insulin,bmi_index, DiabetesPedigreeFunction, Age]
+        diab_prediction = diabetes_model.predict([inputFeatures])
+
+        if (diab_prediction[0] == 1):
+            diab_diagnosis = "The person is Diabetic"
+        else:
+            diab_diagnosis = "The person is not Diabetic"
+
+    st.success(diab_diagnosis)
 
 # Heart Disease Prediction Rate
-if (selected=="Heart Disease Prediction System"):
+if (selected == "Heart Disease Prediction System"):
 
-    #page title
+    # page title
     st.title('Heart Disease Prediction System')
 
 # Parkinsons Disease Prediction Rate
-if (selected=="Parkinsons Disease Prediction System"):
+if (selected == "Parkinsons Disease Prediction System"):
 
-    #page title
+    # page title
     st.title('Parkinsons Disease Prediction System')

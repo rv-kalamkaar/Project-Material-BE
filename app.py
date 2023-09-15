f5f1182db5517cd20c303a51f22fde2abb6a0195
Kunalsrp <pattanshettikunal0201@gmail.comgit config --global user.name KUnalsrp>
2023-09-15 21:45:55 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -190,64 +190,107 @@ if (selected=="Heart Disease Prediction System"):
         heart_prediction = heart_disease_model.predict([inputFeatures2])
 
         if (heart_prediction[0]==1):
-            heart_diagnosis = "The person is Diabetic"
+            heart_diagnosis = "The person is affected by heart disease"
         else:
-            heart_diagnosis = "The person is not Diabetic"
+            heart_diagnosis = "The person is not affected by heart disease"
 
     st.success(heart_diagnosis)
 
 
     #*****************************************PARKISON SECTION********************************************
 
-# Parkinsons Disease Prediction Rate
-if (selected=="Parkinsons Disease Prediction System"):
 
-    #page title
-    st.title('Parkinsons Disease Prediction System')
+# Parkinsons Prediction Page  
+if (selected == 'Parkinsons Disease Prediction System'):    
+    
+    # page title
+    st.title("Parkinsons Disease Prediction System")
 
-    #getting input data from the user
-    #Columns for input fields
-
-    col1, col2, col3 = st.columns(3)
-
+    col1, col2, col3, col4, col5 = st.columns(5)  
+    
     with col1:
-        Pregnancies = st.number_input('No of pregnancies')
-
+        fo = st.text_input('MDVP: Fo(Hz)')
+        
     with col2:
-        Glucose = st.number_input('Glucose Level')
-
+        fhi = st.text_input('MDVP: Fhi(Hz)')
+        
     with col3:
-        BloodPressure = st.number_input('Blood Pressure value')
-
+        flo = st.text_input('MDVP: Flo(Hz)')
+        
+    with col4:
+        Jitter_percent = st.text_input('MDVP: Jitter(%)')
+        
+    with col5:
+        Jitter_Abs = st.text_input('MDVP: Jitter(Abs)')
+        
     with col1:
-        Skinthickness = st.text_input('Skin Thickness value')
-
+        RAP = st.text_input('MDVP: RAP')
+        
     with col2:
-        Insulin = st.text_input('Insulin Level')
-
+        PPQ = st.text_input('MDVP: PPQ')
+        
     with col3:
-        bmi_index = st.text_input('BMI value')
-
+        DDP = st.text_input('Jitter: DDP')
+        
+    with col4:
+        Shimmer = st.text_input('MDVP: Shimmer')
+        
+    with col5:
+        Shimmer_dB = st.text_input('MDVP: Shimmer(dB)')
+        
     with col1:
-        DiabetesPedigreeFunction = st.text_input("Diabetes Pedigree Function value")
-
+        APQ3 = st.text_input('Shimmer: APQ3')
+        
     with col2:
-        Age = st.text_input("Age of a person")
-
-
-
-
-    #code for prediction
-    diab_diagnosis = ' '
-
-    #creating a button for prediction
-
-    if st.button('Diabetes Test Result'):
-        diab_prediction = diabetes_model.predict([['Pregnancies','Glucose','BloodPressure','Skinthickness','Insulin','BMI','DiabetesPedigreeFunction','Age']])
-
-        if (diab_prediction[0]==1):
-            diab_diagnosis = "The person is Diabetic"
+        APQ5 = st.text_input('Shimmer: APQ5')
+        
+    with col3:
+        APQ = st.text_input('MDVP: APQ')
+        
+    with col4:
+        DDA = st.text_input('Shimmer: DDA')
+        
+    with col5:
+        NHR = st.text_input('NHR')
+        
+    with col1:
+        HNR = st.text_input('HNR')
+        
+    with col2:
+        RPDE = st.text_input('RPDE')
+        
+    with col3:
+        DFA = st.text_input('DFA')
+        
+    with col4:
+        spread1 = st.text_input('spread1')
+        
+    with col5:
+        spread2 = st.text_input('spread2')
+        
+    with col1:
+        D2 = st.text_input('D2')
+        
+    with col2:
+        PPE = st.text_input('PPE')
+        
+    
+    
+    # code for Prediction
+    parkinsons_diagnosis = ''
+    
+    # creating a button for Prediction    
+    if st.button("Parkinson's Test Result"):
+        if not all([fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ, DDP, Shimmer, Shimmer_dB, APQ3, APQ5, APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]):
+            st.warning("Please fill in all the fields.")
         else:
-            diab_diagnosis = "The person is not Diabetic"
+            parkinsons_prediction = parkinsons_model.predict([[fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ,DDP,Shimmer,Shimmer_dB,APQ3,APQ5,APQ,DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE]])                          
+            
+            if (parkinsons_prediction[0] == 1):
+              parkinsons_diagnosis = "The person has Parkinson's disease"
+            else:
+              parkinsons_diagnosis = "The person does not have Parkinson's disease"
+        
+    st.success(parkinsons_diagnosis)    
 
-    st.success(diab_diagnosis)
+    

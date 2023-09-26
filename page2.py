f274bdefd6f9120b37e399aa44ce6367a1e42fdd
rv-kalamkaar <rohankumarkhare@gmail.com>
2023-09-26 19:23:27 +0530

    modified:   __pycache__/page1.cpython-39.pyc 	modified:   __pycache__/page2.cpython-39.pyc 	modified:   app.py 	modified:   page1.py 	modified:   page2.py

--- page2.py
+++ page2.py
@@ -1,8 +1,11 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+import pickle as pkl
 
 def main():
     st.title("For Users")
     st.write("This is page 2.")
+
 
 if __name__ == "__main__":
     main()
